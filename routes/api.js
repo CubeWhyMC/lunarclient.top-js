324a9d661a8fc9c9602864ad2e7f4b597ee717e4
cubewhy <cubewhy@163.com>
2024-02-06 11:33:58 +0800

    fix encoding

--- routes/api.js
+++ routes/api.js
@@ -245,7 +245,7 @@ router.post("/launcher/uploadCrashReport", (req, res) => {
 
 router.get("/game/metadata", (req, res) => {
     // todo 动态生成游戏元数据
-    let gameMetadata = fs.readFileSync("config/game-metadata.json");
+    let gameMetadata = fs.readFileSync("config/game-metadata.json", "utf-8");
     res.json(gameMetadata)
 })
 
