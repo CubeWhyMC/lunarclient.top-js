3e74a56bb6e23888cdcbae548ba5f40244393ac1
cubewhy <cubewhy@163.com>
2024-02-08 09:20:30 +0800

    header

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger(":req[X-Forwarded-Host] :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status :res[content-length] \":referrer\""));
+app.use(logger(":req[X-Real-IP] :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status :res[content-length] \":referrer\""));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
